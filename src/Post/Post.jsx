1c170686bc9f9b1ad5b770258c3c0d59238a9cf5
Bruno Molteni <brunomolteni90@gmail.com>
2021-03-16 21:51:49 +0100

    Added responsive info bar

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -7,10 +7,28 @@ import Reactions from "../common/Reactions";
 
 const Post = ({ data }) => {
   const modifiedMarkup = modifyGistEmbed(data.markup);
+  const formattedDate = new Date(data.publishedAt * 1000).toLocaleDateString(
+    "en-us",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
 
   return (
     <main className="post">
       <h1 className="post__title">{data.title} </h1>
+      <div className="post__info">
+        <p>
+          {formattedDate}.{" "}
+          <small>Estimated Time: {data.estimatedTime} Minutes</small>
+        </p>
+        <Reactions
+          reactions={data.reactions.storyReactions}
+          total={data.reactions.total}
+        />
+      </div>
       <img
         className="post__image"
         src={data.mainImage}
